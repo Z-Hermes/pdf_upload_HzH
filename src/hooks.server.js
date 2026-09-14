c22932f32403aa64f22cac58795b01d810156bfc
zhaklinashelqetja <234790815+zhaklinashelqetja@users.noreply.github.com>
2026-09-14 10:40:24 +0200

    auth.js hinzugefügt, hooks.server.js vereinfacht

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,18 +1,16 @@
-// Funktion importieren, die prüft, ob eine Session gültig ist
-import { validateSession } from '$lib/server/auth';
+import { getSession } from '$lib/server/auth.js';
 
-
-// Diese Funktion wird bei jeder Anfrage ausgeführt
 export async function handle({ event, resolve }) {
-
-	// Session-Cookie aus dem Browser lesen
 	const sessionId = event.cookies.get('session');
+	event.locals.user = await getSession(sessionId);
 
-	// Falls eine Session-ID vorhanden ist:
-	// Benutzer aus der Datenbank laden
-	// Sonst user = null setzen
-	event.locals.user = sessionId
-		? await validateSession(sessionId) : null;
+	const path = event.url.pathname;
+	if (path.startsWith('/dashboard') && event.locals.user?.role !== 'user') {
+		return new Response(null, { status: 303, headers: { location: '/login' } });
+	}
+	if (path.startsWith('/admin') && event.locals.user?.role !== 'admin') {
+		return new Response(null, { status: 303, headers: { location: '/login' } });
+	}
 
 	return resolve(event);
 }
